9b8b202b7ba499a08d1a82bae28bf9d76daf0864
asikes <andrew.sikes@dairy.com>
2015-02-26 10:17:45 -0600

    fixing schedule commissioner add

--- src/main/java/com/vasa/scheduling/interfaces/web/ScheduleController.java
+++ src/main/java/com/vasa/scheduling/interfaces/web/ScheduleController.java
@@ -326,6 +326,8 @@ public class ScheduleController extends DefaultHandlerController {
 			return "login";
 		}
 		
+		String listDate = null; 
+		
 		Team t = null;
 		if(teamId != null){
 			t=teamService.findById(Integer.valueOf(teamId));
@@ -349,6 +351,14 @@ public class ScheduleController extends DefaultHandlerController {
 				}
 				service.save(schedule);
 			}
+			
+			Calendar sunday = Calendar.getInstance();
+			sunday.setTime(calendarDay);
+			sunday.set(Calendar.MINUTE, 0);
+			sunday.set(Calendar.HOUR, 0);
+			sunday.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+			
+			listDate = formatter.format(sunday.getTime());
 			
 		} catch (ParseException e) {
 			model.addAttribute("error", e.getMessage());
